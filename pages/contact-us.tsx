9aca1652efac807aefb6d0d9467c5a134d331951
Richard George <richard.jhn.george.3@gmail.com>
2022-12-19 14:35:44 +0000

    page title updates

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -4,8 +4,8 @@ import styles from "../styles/Home.module.css";
 export default function contactUs(): JSX.Element {
   return (
     <div className={styles.container}>
-      <Layout title="Richard's contact us page">
-        <p>Email on rjg.test.website@gmail.com</p>
+      <Layout title="lottogamer.co.uk contact us page">
+        <p>Email on contact@lottogamer.co.uk</p>
         <p>Phone on +44 (0) 1234 567890</p>
         <p>Post at 92 The Road, The Town, The Country. CO2 1NT</p>
       </Layout>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 export default function Home(): JSX.Element {
   return (
     <div className={styles.container}>
-      <Layout title="Richard's cool test website">
+      <Layout title="lottogamer.co.uk - the coolest site for lotto games!">
         <p>this is content</p>
       </Layout>
     </div>
