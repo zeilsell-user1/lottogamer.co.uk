977341feba94f7f628c3969fa2eed5b7df6a0a66
Richard George <richard.jhn.george.3@gmail.com>
2023-03-24 14:38:06 +0000

    changes the meta to support mobile

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -114,7 +114,7 @@ const Layout = ({
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="width=device-width" />
         <meta
           name="description"
           content="Richard's test website using React and Next"
