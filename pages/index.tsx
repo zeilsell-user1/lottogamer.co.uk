dffa9323ec1f5b31b50884c46246fcb6ac379866
Richard George <richard.jhn.george.3@gmail.com>
2023-03-24 12:17:36 +0000

    Added cross sell tile on front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-//import { CrossSellTile, Size, AspectRatio } from "@zeilsell-user1/cross-sell-component";
+import { AspectRatio, CrossSellTile, Size } from "@zeilsell-user1/cross-sell-component";
 import Layout from "../src/components/layout/layout";
 import styles from "../styles/Home.module.css";
 
@@ -6,23 +6,28 @@ export default function Home(): JSX.Element {
   return (
     <div className={styles.container}>
       <Layout title="lottogamer.co.uk - the coolest site for lotto games!">
-        <p>this is content</p>
+        <div className={styles.page}>
+          <h1>Welcome to lottogamer.co.uk!</h1>
+          <h2>We are the UK's #1 place to find games, tools and fun based on lotto</h2>
+          <p>Do you want to know what are your lucky numbers? How about the chances of you winning?</p>
+          <p>We are NOT a gambling site!</p>
+        </div>
+        <div className={styles.page}>
+          <CrossSellTile
+            imageUrl="./bingo-7318132_1280.jpg"
+            targetUrl="product/lotto"
+            title="Feeling lucky today?"
+            titleColor="orange"
+            strapline="Try out our amazing lotto tools!"
+            straplineColor="darkorange"
+            description="limited time offer that is applicable to monkeys only. Do not apply if you are green or over 300"
+            descriptionColor="white"
+            size={Size.large}
+            ratio={AspectRatio.widescreen}
+          />
+        </div>
       </Layout>
     </div>
   );
 }
 
-/*
-
-        <CrossSellTile
-          targetUrl="/product/lotto"
-          title="Feel Lucky?"
-          titleColor="orange"
-          strapline="Try out our amazing lotto tools!"
-          straplineColor="darkorange"
-          ctaText="Play!"
-          ctaTextColour="blue"
-          size={Size.medium}
-          ratio={AspectRatio.widescreen}
-          imageUrl='./bingo-7318132_1280.jpg' />
-          */
